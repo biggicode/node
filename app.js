21c743ed620383f46592bab77e979128e18ad1f0
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-08-17 18:08:51 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//Modules
+
+const audi = "Audi";
+const renault = "Renault";
+
+const startCard = (car) => {
+  console.log(`${car} has started!`);
+};
+
+startCard("Vw");
+startCard(audi);
+startCard(renault);
