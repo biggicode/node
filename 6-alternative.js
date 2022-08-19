d3083419886a2b8d44783253c104a3b6e42b37b0
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-08-19 21:22:55 +0300

    alternative syntax

---
+++ 6-alternative.js
@@ -0,0 +1,5 @@
+module.exports.item = ["item1", "mere", "pere"];
+const person = {
+  hands: 2,
+};
+module.exports.person = person;
--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //Modules
 const cars = require("./4-firstModule");
 const startCar = require("./5-secondMdule");
+const portocale = require("./6-alternative");
 
+console.log(portocale);
 startCar("Vw");
 startCar(cars.audi);
 startCar(cars.renault);
