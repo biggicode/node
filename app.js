2e9cd720df81c4d2e94cb10882885c92bda8ffc8
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-10-24 20:41:38 +0300

    http module

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
-const { readFile, writeFile } = require("fs");
+const http = require("http");
 
-readFile("./content/first.txt", "utf8", (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  const first = result;
-  readFile("./content/second.txt", "utf8", (err, result) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const second = result;
-    writeFile(
-      "./content/result-async.txt",
-      `Async result: ${first} ${second}`,
-      (err, result) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log(result);
-      }
-    );
-  });
+const server = http.createServer((req, res) => {
+  res.write("Welcome to  my first server");
+  res.end();
 });
+
+server.listen(5000);
